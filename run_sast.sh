7063670823e6f0790eaa0e0b27d4f6821a0dcc65
dwwatk02 <87448112+dwwatk02@users.noreply.github.com>
2025-02-04 12:53:35 -0500

    Add files via upload

--- run_sast.sh
+++ run_sast.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 # Downloading and preparing SAClientUtil
 
-#$serviceUrl = "cloud.appscan.com"
+serviceUrl = "cloud.appscan.com"
 if ! [ -x "$(command -v appscan.sh)" ]; then
   echo 'appscan.sh is not installed.' >&2
-  curl -k  "https://cloud.appscan.com/api/v4/Tools/SAClientUtil?os=linux" > SAClientUtil.zip
+  curl -k  "https://$serviceUrl/api/v4/Tools/SAClientUtil?os=linux" > SAClientUtil.zip
   unzip SAClientUtil.zip -d . > /dev/null
   rm -f SAClientUtil.zip
   mv ./SAClientUtil.* ./SAClientUtil
@@ -12,3 +13,20 @@ fi
 
 appscan.sh version
 appscan.sh prepare
+
+
+# Authenticate in ASOC
+asocToken=$(curl -k -s -X POST --header 'Content-Type:application/json' --header 'Accept:application/json' -d '{"KeyId":"'"$asocApiKeyId"'","KeySecret":"'"$asocApiKeySecret"'"}' "https://$serviceUrl/api/v4/Account/ApiKeyLogin" | grep -oP '(?<="Token":\ ")[^"]*')
+if [ -z "$asocToken" ]; then
+  echo "The token variable is empty. Check the authentication process.";
+    exit 1
+fi
+
+irxFile=$(ls -t *.irx | head -n1)
+# Upload IRX file
+if [ -f "$irxFile" ]; then
+    irxFileId=$(curl -k -s -X 'POST' "https://$serviceUrl/api/v4/FileUpload" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" -H 'Content-Type:multipart/form-data' -F "uploadedFile=@$irxFile" | grep -oP '(?<="FileId":\ ")[^"]*');
+    echo "$irxFile exist. It will be uploaded to ASoC. IRX file id is $irxFileId.";
+else
+    echo "IRX file not identified.";
+fi
