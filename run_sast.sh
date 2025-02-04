4836f0fa521846dcb14d61acd7b4c6eb571b3d1d
dwwatk02 <87448112+dwwatk02@users.noreply.github.com>
2025-02-04 13:05:22 -0500

    Add files via upload

--- run_sast.sh
+++ run_sast.sh
@@ -4,6 +4,7 @@
 serviceUrl="cloud.appscan.com"
 asocApiKeyId="4a6dec6b-2ae7-8adb-eff7-a1903dea651c"
 asocApiKeySecret="GzVrXNG/bXvOm35wWOL/oCOkxHdzyZhDyuu6hk8uMIpP"
+appId="4d5b6b9b-ec4c-4f98-b16b-b8390c7fa2d9"
 
 if ! [ -x "$(command -v appscan.sh)" ]; then
   echo 'appscan.sh is not installed.' >&2
@@ -33,3 +34,28 @@ if [ -f "$irxFile" ]; then
 else
     echo "IRX file not identified.";
 fi
+
+# Start scan
+scanId=$(curl -s -k -X 'POST' "https://$serviceUrl/api/v4/Scans/Sast" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" -H 'Content-Type:application/json' -d '{"AppId":"'"$appId"'","ApplicationFileId":"'"$irxFileId"'","ClientType":"user-site","EnableMailNotification":false,"Execute":true,"Locale":"en","Personal":false,"ScanName":"'"SAST $scanName $irxFile"'","EnablementMessage":"","FullyAutomatic":true}'| jq -r '. | {Id} | join(" ")');
+echo "Scan started, scanId $scanId"
+
+echo "The scan name is $scanName and scanId is $scanId"
+echo $scanId > scanId.txt
+
+# Check status scan and keep it in loop until Ready status.
+scanStatus=$(curl -k -s -X 'GET' "https://$serviceUrl/api/v4/Scans/Sast/$scanId" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" | jq -r '.LatestExecution | {Status} | join(" ")');
+echo $scanStatus
+while true ; do 
+    scanStatus=$(curl -k -s -X 'GET' "https://$serviceUrl/api/v4/Scans/Sast/$scanId" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" | jq -r '.LatestExecution | {Status} | join(" ")');
+    if [ "$scanStatus" == "Running" ] || [ "$scanStatus" == "InQueue" ]; then
+        echo $scanStatus
+    elif [ "$scanStatus" == "Failed" ]; then
+        echo $scanStatus
+        echo "Scan Failed. Check ASOC logs"
+        exit 1
+    else
+        echo $scanStatus
+        break
+    fi
+    sleep 60
+done
