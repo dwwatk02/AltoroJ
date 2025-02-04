a62a1c0c16cff1da9823a7c35f681eadd3ed7ed8
dwwatk02 <87448112+dwwatk02@users.noreply.github.com>
2025-02-04 12:37:37 -0500

    Add files via upload

--- run_sast.sh
+++ run_sast.sh
@@ -1,9 +1,9 @@
 # Downloading and preparing SAClientUtil
 
-$serviceUrl = "cloud.appscan.com"
+#$serviceUrl = "cloud.appscan.com"
 if ! [ -x "$(command -v appscan.sh)" ]; then
   echo 'appscan.sh is not installed.' >&2
-  curl -k  "https://$serviceUrl/api/v4/Tools/SAClientUtil?os=linux" > SAClientUtil.zip
+  curl -k  "https://cloud.appscan.com/api/v4/Tools/SAClientUtil?os=linux" > SAClientUtil.zip
   unzip SAClientUtil.zip -d . > /dev/null
   rm -f SAClientUtil.zip
   mv ./SAClientUtil.* ./SAClientUtil
